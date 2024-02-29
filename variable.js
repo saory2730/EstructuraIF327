b1bb858d90df0884ec63833d60cf34419b4e2067
saory2730 <161676850+saory2730@users.noreply.github.com>
2024-02-28 23:54:56 -0500

    Se hace ahuste ded

--- variable.js
+++ variable.js
@@ -1,6 +1,8 @@
-var a
+/*var a
 var b
 a=parseFloat(prompt("ingrese un valor"+"  "+ a))
 b=parseFloat(prompt("ingrese un valor"+"  "+ b))
 c=a+b
-alert("el valor de la suma es"+"  "+  c)
+alert("el valor de la suma es"+"  "+  c) 
+*/
+alert ("esto lo agregué desde remoto")
